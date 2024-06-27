dc6c99a59f4795d89ce32476fc4720bffd9993d0
jan438 <jan438@users.noreply.github.com>
2024-06-27 15:13:07 +0200

    trigger tijd  60s

--- cleannewics.py
+++ cleannewics.py
@@ -18,7 +18,7 @@ def find_all_occurrences(line, sub, f, t):
 def process_alarm(line, pos, endpos):
     alarmtext = line[pos:] + line[:endpos]
     decodedtext = alarmtext.decode("utf-8")
-    print("Trigger", len(alarmtext), decodedtext[0], decodedtext[1], decodedtext[2])
+    print("Trigger", len(alarmtext), decodedtext[25], decodedtext[26], decodedtext[27])
     processed = line[:pos] + "A[123]".encode() + crlf + line[endpos+12:]
     return processed
 
