bc7dbebf6be22b903c59f2c464bd01405904e825
jan438 <jan438@users.noreply.github.com>
2024-07-20 10:24:13 +0200

    print relocated

--- ics2pdf.py
+++ ics2pdf.py
@@ -188,8 +188,8 @@ def combinecolumns(prm1, prm2, alarm):
     paragraph = Paragraph(processed, matrixtimlocStyle )
     return paragraph
     
-def placeimage():
-    print("PlaceImage")
+def placeimage(countevents):
+    print("PlaceImage", countevents)
     return
     
 def processwdescription(textpar):
@@ -260,8 +260,7 @@ def fillWeekReports(first_week, countdays):
                 weekreps[i].append0_Paragraph(monthevents[j].weekday, monthevents[j].location, weeklocStyle)
                 weekreps[i].append0_Paragraph(monthevents[j].weekday, monthevents[j].description, weekdesStyle)
                 countevents[monthevents[j].weekday] += 1
-        print(countevents)
-        placeimage()
+        placeimage(countevents)
         key = input()
         wk += 1
         if wk < last_week - first_week + 1:
@@ -278,8 +277,7 @@ def fillWeekReports(first_week, countdays):
                     weekreps[i].append1_Paragraph(monthevents[j].weekday, monthevents[j].location, weeklocStyle)
                     weekreps[i].append1_Paragraph(monthevents[j].weekday, monthevents[j].description, weekdesStyle)
                     countevents[monthevents[j].weekday] += 1
-            print(countevents)
-            placeimage()
+            placeimage(countevents)
             key = input()
         tbl_data = [
         [weekreps[i].h0[0], weekreps[i].h0[1], weekreps[i].h0[2], weekreps[i].h0[3], weekreps[i].h0[4], weekreps[i].h0[5], weekreps[i].h0[6]],
