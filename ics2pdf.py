b37feacb43d2d971d446a133ae7ccacbe543c506
jan438 <jan438@users.noreply.github.com>
2024-07-22 09:39:41 +0200

    titel changed

--- ics2pdf.py
+++ ics2pdf.py
@@ -37,7 +37,7 @@ matrixsumheadingStyle = ParagraphStyle('sum', parent=styles['Normal'], fontName 
 matrixsumStyle = ParagraphStyle('sum', parent=styles['Normal'], fontName = calfont + "Bold", fontSize = 10, spaceBefore = 0, spaceAfter = 1, textColor = green, alignment=TA_CENTER, leading = 8)
 matrixdesStyle = ParagraphStyle('des', parent=styles['Normal'], fontName = calfont, fontSize = 8, spaceBefore = 1, spaceAfter = 2, textColor = purple, alignment=TA_CENTER, leading = 8)
 matrixtimlocStyle = ParagraphStyle('tim', parent=styles['Normal'], fontName = calfont, fontSize = 8, spaceBefore = 3, spaceAfter = 1, textColor = red, alignment=TA_CENTER, leading = 8)
-title = Paragraph("Juni 2024", titleStyle)
+title = Paragraph("Juli 2024", titleStyle)
 weekdaynames = ["Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag","Zondag"]
 monthnames = ["Januari","Februari","Maart","April","Mei","Juni","Juli","Augustus", "September","Oktober","November","December"]
 weekStyle = [
