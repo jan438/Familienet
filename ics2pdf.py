8e7039d79f1cb836f7eaae152aab582130df672f
jan438 <jan438@users.noreply.github.com>
2024-07-31 14:27:46 +0200

    verdana italic used

--- ics2pdf.py
+++ ics2pdf.py
@@ -21,7 +21,7 @@ from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER
 
 startdate = date(1990,1,1)
 datecal = datetime.now()
-calfont = "Georgia"
+calfont = "Verdana"
 weekreps = []
 columsmatrixreport = 3
 rowsmatrixreport = 4
@@ -628,6 +628,7 @@ pdfmetrics.registerFont(TTFont('ArialBold', 'Arial_Bold.ttf'))
 pdfmetrics.registerFont(TTFont('ArialItalic', 'ariali.ttf'))
 pdfmetrics.registerFont(TTFont('Verdana', 'Verdana.ttf'))
 pdfmetrics.registerFont(TTFont('VerdanaBold', 'Verdana_Bold.ttf'))
+pdfmetrics.registerFont(TTFont('VerdanaItalic', 'verdanai.ttf'))
 pdfmetrics.registerFont(TTFont('Georgia', 'Georgia.ttf'))
 pdfmetrics.registerFont(TTFont('GeorgiaBold', 'Georgia_Bold.ttf'))
 pdfmetrics.registerFont(TTFont('GeorgiaItalic', 'georgiai.ttf'))
