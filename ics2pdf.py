8cd299281abc7fd9ddecbae380064b3a37ad77d9
jan438 <jan438@users.noreply.github.com>
2024-08-03 14:01:28 +0200

    georgia bold italic added

--- ics2pdf.py
+++ ics2pdf.py
@@ -21,7 +21,7 @@ from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER
 
 startdate = date(1990,1,1)
 datecal = datetime.now()
-calfont = "Trebuchet"
+calfont = "Georgia"
 weekreps = []
 columsmatrixreport = 3
 rowsmatrixreport = 4
@@ -634,7 +634,8 @@ pdfmetrics.registerFont(TTFont('VerdanaBold', 'Verdana_Bold.ttf'))
 pdfmetrics.registerFont(TTFont('VerdanaItalic', 'verdanai.ttf'))
 pdfmetrics.registerFont(TTFont('Georgia', 'Georgia.ttf'))
 pdfmetrics.registerFont(TTFont('GeorgiaBold', 'Georgia_Bold.ttf'))
-pdfmetrics.registerFont(TTFont('GeorgiaItalic', 'georgiai.ttf'))
+pdfmetrics.registerFont(TTFont('GeorgiaItalic', 'Georgia_Italic.ttf'))
+pdfmetrics.registerFont(TTFont('GeorgiaBoldItalic', 'Georgia_Bold_Italic.ttf'))
 pdfmetrics.registerFont(TTFont('TimesNewRoman', 'times.ttf'))
 pdfmetrics.registerFont(TTFont('TimesNewRomanBold', 'timesbd.ttf'))
 pdfmetrics.registerFont(TTFont('TimesNewRomanItalic', 'timesi.ttf'))
