8433fb14b14207f46bd4b34ef70e282e8242966d
jan438 <jan438@users.noreply.github.com>
2024-08-04 09:01:19 +0200

    photo added

--- ics2pdf.py
+++ ics2pdf.py
@@ -209,6 +209,7 @@ def lookupimage(imgcode):
     newcalendar[171] = "Photos/MiniPrideWalk12juni-2.jpg"
     newcalendar[191] = "Photos/6617f09c1049a.jpg"
     newcalendar[190] = "Photos/keukenhof-lisse-molen.jpg"
+    newcalendar[262] = "Photos/spain_PNG58.png"
     newcalendar[266] = "Photos/AlpenWeide.jpeg"
     newcalendar[269] = "Photos/Los-del-Sol-cantando-mariachi.jpg"
     img = newcalendar[int(imgcode)]
