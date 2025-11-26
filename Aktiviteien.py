b47f4bfec25678f990c6ebd12971674a12951688
jan438 <jan438@users.noreply.github.com>
2025-11-26 16:30:42 +0100

    arc corrected

--- Aktiviteien.py
+++ Aktiviteien.py
@@ -227,8 +227,8 @@ activity_x = activity_x + 100
 activity_y = activity_y + 0
 p = c.beginPath()
 p.moveTo(activity_x + 100, activity_y + 0)
-p.lineTo(activity_x + 200, activity_y + 0)
-p.arcTo( activity_x + 200, activity_y + 0,   activity_x + 100, activity_y + 50, startAng=0, extent=90)
+p.lineTo(activity_x + 210, activity_y + 0)
+p.arcTo( activity_x + 200, activity_y + 10,   activity_x + 100, activity_y + 50, startAng=0, extent=90)
 p.lineTo(activity_x + 100, activity_y + 0)
 c.drawPath(p, stroke=1, fill=1)
 activity_x = 250
