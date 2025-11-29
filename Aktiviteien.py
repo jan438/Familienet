845ac7fe4c8213182e0e2dd0f32e262dc8f10eb4
jan438 <jan438@users.noreply.github.com>
2025-11-29 14:45:55 +0100

    bijna circel rond

--- Aktiviteien.py
+++ Aktiviteien.py
@@ -98,7 +98,7 @@ def drawactivityA(c, activity_x, activity_y, w, h, a):
     p.lineTo(activity_x + w + a, activity_y + h)                                                      # vertcal line
     p.arcTo(activity_x + w, activity_y + h, activity_x + w + a, activity_y + h + a, startAng = 0, extent = 90)     # arc right above
     p.lineTo(activity_x + 0.5 * a, activity_y + h + a)                                                   # horizontal line
-    p.arcTo(activity_x, activity_y, activity_x + a, activity_y + a, startAng = 90, extent = 90)        # arc left above
+    p.arcTo(activity_x, activity_y + h, activity_x + a, activity_y + h + a, startAng = 90, extent = 90)    # arc left above
     p.lineTo(activity_x, activity_y)                                                                # vertcal line
     c.drawPath(p, stroke=1, fill=1)
     return
