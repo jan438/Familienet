b26af5767fb0b632f8d6b3fd99b2e04e25120ad8
jan438 <jan438@users.noreply.github.com>
2025-12-01 14:04:49 +0100

    activit_kind coordinates added

--- Aktiviteien.py
+++ Aktiviteien.py
@@ -114,8 +114,8 @@ def drawActivity(c, activity_x, activity_y, w, h, a, i):
     if len(imgcode) > 0:
         img = lookupimage(imgcode)
         if len(img) > 0:
-            c.radialGradient(activity_x, activity_y, 100, (pinkredcircle, orangecircle), extend = False)
-            c.circle(activity_x, activity_y, 100, stroke = 0)
+            c.radialGradient(activity_x + activity_kind_x, activity_y + activity_kind_y, activity_kind_r, (pinkredcircle, orangecircle), extend = False)
+            c.circle(activity_x + activity_kind_x, activity_y + activity_kind_y, activity_kind_r, stroke = 0)
             drawing = scaleSVG("SVG/" + img + ".svg", 0.5)
             renderPDF.draw(drawing, c, activity_x + 5, activity_y + 5)
     return
