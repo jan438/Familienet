83185b92570a0503bc0b9a8386055b272af6cb1d
jan438 <jan438@users.noreply.github.com>
2025-12-02 14:07:34 +0100

    truncated added

--- Aktiviteien.py
+++ Aktiviteien.py
@@ -127,7 +127,10 @@ def drawActivity(c, activity_x, activity_y, w, h, a, i):
     occurrences = find_all_occurrences(monthevents[i].summary, " ", 0, len(monthevents[i].summary))
     summarywidth = pdfmetrics.stringWidth(monthevents[i].summary, activityfont, 10)
     print(monthevents[i].summary, len(occurrences), "summarywidth", round(summarywidth))
-    c.drawString(activity_x + 5, activity_y + 50, monthevents[i].summary)
+    if summarywidth < 100:
+        c.drawString(activity_x + 5, activity_y + 50, monthevents[i].summary)
+    else:
+        c.drawString(activity_x + 5, activity_y + 50, "te lang")
     imgcode = processsdescription(monthevents[i].description)
     activity_kind_x = 75
     activity_kind_y = 100
