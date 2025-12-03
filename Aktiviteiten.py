383ceeee255c04244aa9c30ea60ce8abed5b825f
jan438 <jan438@users.noreply.github.com>
2025-12-03 11:17:58 +0100

    Update Aktiviteiten.py

--- Aktiviteiten.py
+++ Aktiviteiten.py
@@ -96,6 +96,7 @@ def lookupimage(imgcode):
     newcalendar[14] = "bibliotheek"
     newcalendar[30] = "wandelen"
     newcalendar[31] = "sportenspelmiddag"
+    newcalendar[32] = "wenskaarten"
     newcalendar[33] = "bloemschikken"
     newcalendar[34] = "klassiekemuziek"
     img = newcalendar[int(imgcode)]
