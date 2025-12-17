2215a04b420e48e9476c3c339841c4999bcb1fa8
jan438 <jan438@users.noreply.github.com>
2025-12-17 15:18:21 +0100

    rect iets smaller

--- Aktiviteiten.py
+++ Aktiviteiten.py
@@ -359,7 +359,7 @@ activity_height = 90
 activity_angle = 20
 col = 0
 for i in range(0, len(monthevents)):
-    drawRect(c, activity_x - 10,  activity_y - 10, activity_width + 30, activity_height + 40, activity_angle, lighteryellow)
+    drawRect(c, activity_x - 10,  activity_y - 10, activity_width + 20, activity_height + 30, activity_angle, lighteryellow)
     drawActivity(c, activity_x,  activity_y, activity_width, activity_height, activity_angle, i)
     col += 1
     activity_x = activity_x + 270
