b74ed36ae779998773163ac71d98ae6acb3642fa
jan438 <jan438@users.noreply.github.com>
2026-01-16 09:24:27 +0100

    hexagon added

--- turtletops.py
+++ turtletops.py
@@ -1,7 +1,19 @@
+# import the turtle modules
 import turtle
-t = turtle.Turtle()
-canvas = t.getscreen().getcanvas()
 
-canvas.postscript(file="PDF/turtle_img.ps")
+# Start a work Screen
+ws = turtle.Screen()
+
+# Define a Turtle Instance
+geekyTurtle = turtle.Turtle()
+
+# executing loop 6 times for 6 sides
+for i in range(6):
+
+    # Move forward by 90 units
+    geekyTurtle.forward(90)
+
+    # Turn left the turtle by 300 degrees
+    geekyTurtle.left(300)
 
 key = input("Wait")
